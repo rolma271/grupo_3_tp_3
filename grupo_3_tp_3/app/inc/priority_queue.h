53639376c2462a28f4d6d45bcfd89267eebacf25
rolma271 <rolma271@gmail.com>
2024-08-09 23:52:09 -0300

    refactor: priority_queue.h

--- grupo_3_tp_3/app/inc/priority_queue.h
+++ grupo_3_tp_3/app/inc/priority_queue.h
@@ -1,3 +1,22 @@
+/**
+ * @file priority_queue.h
+ * @brief Priority Queue Library for FreeRTOS applications
+ *
+ * This library provides a priority queue implementation suitable for use in FreeRTOS.
+ * The priority queue is implemented as a binary heap and supports operations to
+ * send and receive events with varying priorities. The heap is structured as a max-heap,
+ * where the event with the highest priority is always at the root.
+ *
+ * The library includes functions to create a priority queue, send events to the queue,
+ * and receive events from the queue. The library is thread-safe, utilizing FreeRTOS
+ * mutexes and semaphores to synchronize access to the queue.
+ *
+ * @authors 
+ * - Marco Rolón Radcenco
+ * - Pablo Eduardo Gimenez
+ * - Iván Podoroska
+ */
+
 #ifndef PRIORITY_QUEUE_H
 #define PRIORITY_QUEUE_H
 
@@ -31,7 +50,11 @@ typedef int pq_size_t;
 #define PQ_MAX_EVENT_SIZE 10 /**< Maximum size of the priority queue */
 
 /**
- * @brief Structure representing a priority queue.
+ * @brief Structure representing the priority queue.
+ *
+ * The priority queue is implemented as a binary heap stored in an array. The queue
+ * maintains a size indicating the number of events currently in the queue, and
+ * uses a mutex and semaphore for thread-safe operations.
  */
 typedef struct 
 {
@@ -44,28 +67,41 @@ typedef struct
 pq_handle_t;
 
 /**
- * @brief Create a priority queue.
+ * @brief Creates a new priority queue.
  *
- * @return Pointer to the created priority queue, or NULL if creation failed.
+ * Allocates memory for a new priority queue and initializes its components.
+ * The queue is implemented as a max-heap, where the highest priority event
+ * is always at the root.
+ *
+ * @return Pointer to the newly created priority queue, or NULL if creation failed.
  */
 pq_handle_t *xPriorityQueueCreate(void);
 
 /**
- * @brief Send an event to the priority queue.
+ * @brief Sends an event to the priority queue.
+ *
+ * Inserts an event into the priority queue, maintaining the max-heap property.
+ * The function is thread-safe and will block if the queue is full, until space
+ * becomes available or the specified timeout period expires.
  *
  * @param pq Pointer to the priority queue.
- * @param event Pointer to the event to send to the queue.
- * @param ticksToWait Maximum time to wait for space to become available.
+ * @param event Pointer to an event to send to the queue.
+ * @param ticksToWait Maximum time (in ticks) to wait for space to become available.
  * @return pdPASS if the event was successfully sent, pdFAIL otherwise.
  */
 BaseType_t xPriorityQueueSend(pq_handle_t *pq, pq_event_t *event, TickType_t ticksToWait);
 
 /**
- * @brief Receive an event from the priority queue.
+ * @brief Receives an event from the priority queue.
+ *
+ * Retrieves and removes the highest priority event from the priority queue,
+ * maintaining the max-heap property. The thread-safe function will block
+ * if the queue is empty, until an event becomes available or the specified timeout
+ * period expires.
  *
  * @param pq Pointer to the priority queue.
  * @param event Pointer to store the received event.
- * @param ticksToWait Maximum time to wait for an event.
+ * @param ticksToWait Maximum time (in ticks) to wait for an event to become available.
  * @return pdPASS if an event was successfully received, pdFAIL otherwise.
  */
 BaseType_t xPriorityQueueReceive(pq_handle_t *pq, pq_event_t *event, TickType_t ticksToWait);
